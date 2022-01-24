72dac4e2c0a1728f5b390559b1da4836cc836193
freedom-wy <dazhuang_python@sina.com>
2022-01-24 15:19:11 +0800

    端口扫描masscan

--- sample/handle_subprocess.py
+++ sample/handle_subprocess.py
@@ -1,24 +1,6 @@
 import subprocess
 
-
-def test():
-    process_list = [{subprocess.Popen(["python", "test.py"], stdout=subprocess.DEVNULL,
-                                      stderr=subprocess.DEVNULL): "进程号{}".format(i)} for i in range(1, 4)]
-    while process_list[:]:
-        for item in process_list:
-            for k, v in item.items():
-                process_return = k.poll()
-                if process_return is None:
-                    print("{}正在运行".format(v))
-                elif process_return == 0:
-                    print("{}运行结束".format(v))
-                    process_list.remove(item)
-                elif process_return == 1:
-                    print("{}sleep状态".format(v))
-                elif process_return == 2:
-                    print("{}进程不存在".format(v))
-                elif process_return < 0:
-                    print("{}进程已被杀死".format(v))
-
-
-test()
+process = subprocess.Popen(["python", "test.py"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+while True:
+    process_return = process.poll()
+    print(process_return)
--- w_kuma/port_scanner/port_scan.py
+++ w_kuma/port_scanner/port_scan.py
@@ -4,7 +4,22 @@
 2、调用nmap识别端口开放服务
 """
 
+import os
+import time
+from w_kuma.config import *
+import subprocess
+
 
 class PortScanner(object):
-    def __init__(self):
-        pass
+    def __init__(self, ipinfo, portinfo):
+        self.ipinfo = ipinfo
+        self.portinfo = portinfo
+        self.__work_dir = os.path.dirname(os.path.abspath(__file__))
+        self.__masscan_program = os.path.join(self.__work_dir, "port_resource", "masscan")
+        self.__outfile = "portscan_{time}.xml".format(time=int(time.time()))
+
+    def portscan(self):
+        cmd = "masscan -p {portinfo} {ipinfo} --rate={rate} -oX {outfile}".format(
+            portinfo=self.portinfo, ipinfo=self.ipinfo, rate=masscan_scan_rate, outfile=self.__outfile)
+        process = subprocess.Popen(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        # 判断masscan进程状态
